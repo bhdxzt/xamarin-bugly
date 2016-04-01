51146d889807965b037509a29367fbe0a7362a5b
Zou Jian <chsword@126.com>
2016-04-01 17:15:16 +0800

    update to 2.1

--- agent/buglyAgent/agent/src/main/java/com/chteam/agent/BuglyAgentHelper.java
+++ agent/buglyAgent/agent/src/main/java/com/chteam/agent/BuglyAgentHelper.java
@@ -9,9 +9,21 @@ public class BuglyAgentHelper {
     public static void init(Context context,String appId){
         CrashReport.initCrashReport(context,appId,false);
     }
+
+    public static void init(Context context){
+        CrashReport.initCrashReport(context);
+    }
+
     public static void setUserId(String userId){
         CrashReport.setUserId(userId);
     }
+    public static void setUserSceneTag(Context context,int i){
+        CrashReport.setUserSceneTag(context, i);
+    }
+    public static void putUserData(Context context,String key,String value){
+        CrashReport.putUserData(context, key,value);
+    }
+
     public static void testJavaCrash(){
         CrashReport.testJavaCrash();
     }
