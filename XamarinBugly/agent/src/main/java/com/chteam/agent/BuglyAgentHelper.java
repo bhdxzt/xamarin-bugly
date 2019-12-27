ac14457cd991c0bb8d881ec24fd61cea1f7e727b
Dreamless <DreamlessRaw@163.com>
2019-12-27 10:28:45 +0800

    Update bugly_crashreport_upgrade-1.4.2.aar

---
+++ XamarinBugly/agent/src/main/java/com/chteam/agent/BuglyAgentHelper.java
@@ -0,0 +1,77 @@
+package com.example.bugly;
+
+import android.content.Context;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.beta.UpgradeInfo;
+import com.tencent.bugly.crashreport.*;
+
+/**
+ * Created by MaZhiLun on 2019/12/25.
+ */
+public class BuglyAgentHelper {
+    public static void init(Context context, String appId) {
+        Bugly.init(context, appId, false);
+    }
+
+    public static void init(Context context, String appId, boolean debug) {
+        Bugly.init(context, appId, debug);
+    }
+
+    public static void init(Context context) {
+        CrashReport.initCrashReport(context);
+    }
+
+    public static void setUserId(String userId) {
+        CrashReport.setUserId(userId);
+    }
+
+    public static void setUserSceneTag(Context context, int i) {
+        CrashReport.setUserSceneTag(context, i);
+    }
+
+    public static void putUserData(Context context, String key, String value) {
+        CrashReport.putUserData(context, key, value);
+    }
+
+    public static void testJavaCrash() {
+        CrashReport.testJavaCrash();
+    }
+
+    public static void closeBugly() {
+        CrashReport.closeBugly();
+    }
+
+    public static void closeCrashReport() {
+        CrashReport.closeCrashReport();
+    }
+
+    public static void setAppVersion(Context context, String version) {
+        CrashReport.setAppVersion(context, version);
+    }
+
+    public static void setIsDevelopmentDevice(Context context, boolean isDevelopment) {
+
+        CrashReport.setIsDevelopmentDevice(context, isDevelopment);
+    }
+
+    public static void setIsDevelopmentDevice1(Context context, boolean isForeground) {
+        CrashReport.setIsAppForeground(context, isForeground);
+    }
+
+    public static void postCatchedException(Throwable catchedThrowed) {
+        CrashReport.postCatchedException(catchedThrowed);
+    }
+
+    public static void checkUpgrade() {
+        Beta.checkUpgrade();
+    }
+
+    public static void cancelDownload() {
+        Beta.cancelDownload();
+    }
+
+    public static UpgradeInfo getUpgradeInfo() {
+        return Beta.getUpgradeInfo();
+    }
+}
